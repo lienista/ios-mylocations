ac0ed83f5a72c2f873c1e4b60ca112259a28503a
Lien Nguyen <lienista@Liens-MacBook-Pro.local>
2013-11-21 16:16:25 -0800

    Initial Commit

---
+++ MyLocations/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  MyLocations
+//
+//  Created by Lien Nguyen on 11/21/13.
+//  Copyright (c) 2013 Lienne Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ MyLocations/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  MyLocations
+//
+//  Created by Lien Nguyen on 11/21/13.
+//  Copyright (c) 2013 Lienne Nguyen. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
