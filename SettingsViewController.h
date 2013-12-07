355abc8405441e8b59c057cf60e22d723462f1b9
Lien Nguyen <lienista@Liens-MacBook-Pro.local>
2013-12-07 13:33:41 -0800

    table for settings works

---
+++ SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  MyLocations
+//
+//  Created by Lien Nguyen on 12/7/13.
+//  Copyright (c) 2013 Lienne Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UITableViewController
+
+@end
